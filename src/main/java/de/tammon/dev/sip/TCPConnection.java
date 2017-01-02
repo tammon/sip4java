57ea50d5004b25f5237c7a6e7cfa9f3e8e48bac4
Tammo Schwindt <dev@tammon.de>
2017-01-02 17:33:12 +0100

    added top layer TCPConnection with respective interface

---
+++ src/main/java/de/tammon/dev/sip/TCPConnection.java
@@ -0,0 +1,105 @@
+/*
+ * Sercos Internet Protocol (SIP) version 1 - Java Implementation
+ * Copyright (c) 2017. by tammon (Tammo Schwindt)
+ * This code is licensed under the GNU LGPLv2.1
+ */
+
+package de.tammon.dev.sip;
+
+import de.tammon.dev.sip.packets.Connect;
+import de.tammon.dev.sip.packets.ConnectResponse;
+import de.tammon.dev.sip.packets.Request;
+import de.tammon.dev.sip.packets.Response;
+import de.tammon.dev.sip.packets.parts.Head;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class TCPConnection implements SipConnection {
+    private InetAddress ipAddress;
+    private int maxDelay, leaseTimeout, busyTimeout, sipPort, sipVersion;
+    private int transactionId = 0;
+    private boolean connected = false;
+    private int[] supportedMessages;
+    private Socket socketConnection;
+    private DataOutputStream dataOutputStream;
+    private DataInputStream dataInputStream;
+
+    public TCPConnection(String host) throws Exception {
+
+        // Load Default Properties from properties file and initialize
+        InputStream inputStream = ClassLoader.getSystemResourceAsStream("sipDefault.properties");
+        Properties properties = new Properties();
+        properties.load(inputStream);
+        this.sipPort = new Integer(properties.getProperty("sipPort"));
+        this.ipAddress = InetAddress.getByName((host == null) ? properties.getProperty("driveIp") : host);
+        this.leaseTimeout = new Integer(properties.getProperty("leaseTimeout"));
+        this.busyTimeout = new Integer(properties.getProperty("busyTimeout"));
+        this.maxDelay = new Integer(properties.getProperty("maxDelay"));
+        this.sipVersion = new Integer(properties.getProperty("sipVersion"));
+
+        // Create new Socket Connection
+        this.socketConnection = new Socket();
+        this.socketConnection.connect(new InetSocketAddress(this.ipAddress, this.sipPort), leaseTimeout);
+
+        this.dataOutputStream = new DataOutputStream(this.socketConnection.getOutputStream());
+        this.dataInputStream = new DataInputStream(this.socketConnection.getInputStream());
+
+        this.connectSip();
+    }
+
+    public TCPConnection() throws Exception {
+        this(null);
+    }
+
+    private synchronized int getNewTransactionId (){
+        return this.transactionId++;
+    }
+
+    private synchronized Response tcpSendAndReceive(Request request, Response response) throws Exception {
+        this.dataOutputStream.write(request.getTcpMsgAsByteArray());
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        while (true) {
+            int readLength = dataInputStream.read(buffer);
+            outputStream.write(buffer, 0, readLength + 1);
+            if (readLength <1024) break;
+        }
+
+        byte[] rawResponse = outputStream.toByteArray();
+        Head header = new Head(rawResponse);
+
+        if (header.getTransactionId() != request.getTransactionId()) throw new Exception("The response transaction ID doesn't match the request transaction ID");
+
+        // Check if we got an ExceptionResponse todo: map to exceptionResponse
+        if (header.getMessageType() == 67) throw new Exception("Drive threw Communication Exception");
+        else if (header.getMessageType() == response.getMessageType()){
+            response.setData(rawResponse);
+            return response;
+        } else throw new Exception("Invalid Message Type Response");
+    }
+
+    private void connectSip() throws Exception {
+        Connect request = new Connect(this.getNewTransactionId(), this.sipVersion, this.busyTimeout, this.leaseTimeout);
+        ConnectResponse response = (ConnectResponse) this.tcpSendAndReceive(request, new ConnectResponse());
+        this.supportedMessages = response.getPacketBody().getSupportedMessageTypes();
+        Arrays.stream(this.getSupportedMessages()).forEach(System.out::println);
+        this.connected = true;
+    }
+
+    public boolean isConnected() {
+        return connected && this.socketConnection.isConnected();
+    }
+
+    public int[] getSupportedMessages() {
+        return supportedMessages;
+    }
+}
