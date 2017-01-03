4f2f249cb0872fbfc39619ff37c543b258691d16
Tammo Schwindt <dev@tammon.de>
2017-01-03 14:20:00 +0100

    Updated copyright and license

--- src/main/java/net/tammon/sip/packets/Request.java
+++ src/main/java/net/tammon/sip/packets/Request.java
@@ -1,7 +1,20 @@
 /*
- * Sercos Internet Protocol (SIP) version 1 - Java Implementation
- * Copyright (c) 2017. by tammon (Tammo Schwindt)
- * This code is licensed under the GNU LGPLv2.1
+ * Sercos Internet Protocol (SIP) version 1
+ * Copyright (C) 2017. tammon (Tammo Schwindt)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 package net.tammon.sip.packets;
